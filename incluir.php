5334e0a3cbaa2cd9a0ad92d3086e2d45745311bc
thuliobatista <thulio.batista@rbmweb.com.br>
2022-10-20 20:28:14 -0300

    feature:Integração BD

--- incluir.php
+++ incluir.php
@@ -11,7 +11,8 @@ $querypesquisa = mysqli_query($db,$sql) or die(mysqli_error($db));
 if(mysqli_num_rows($querypesquisa) == 0){
     $sql = "INSERT INTO usuarios (NOME, EMAIL, SENHA) VALUES ('$nome', '$email', '$senha')";
     $queryinclude = mysqli_query($db,$sql) or die(mysqli_error($db));
-    header("location:index.html");
+    $alerta = 0;
+    header("location:index.html?alerta=$alerta");
 }else{
     $alerta = 1;
     header("location:cadastro.html?alerta=$alerta"); 
